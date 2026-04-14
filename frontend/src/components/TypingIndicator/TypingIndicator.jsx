0e2c6b8ceddc9db5a06e331d510b29d298bc31d9
Nurudeen Abdulsalaam <ndabdulsalaam@gmail.com>
2026-04-14 02:42:58 +0100

    refactor: remove duplicate Rx avatar from TypingIndicator

--- frontend/src/components/TypingIndicator/TypingIndicator.jsx
+++ frontend/src/components/TypingIndicator/TypingIndicator.jsx
@@ -3,7 +3,6 @@ import './TypingIndicator.css'
 function TypingIndicator() {
     return (
         <div className="typing">
-            <div className="typing__avatar">Rx</div>
             <div className="typing__bubble">
                 <span className="typing__dot" />
                 <span className="typing__dot" />
