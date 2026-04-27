91edc7290d64f1cfa66480b9e722306df355301b
Nurudeen Abdulsalaam <ndabdulsalaam@gmail.com>
2026-04-27 16:35:04 +0100

    Update/Add rxchat_frontend/src/components/TypingIndicator/TypingIndicator.jsx

---
+++ rxchat_frontend/src/components/TypingIndicator/TypingIndicator.jsx
@@ -0,0 +1,15 @@
+import './TypingIndicator.css'
+
+function TypingIndicator() {
+    return (
+        <div className="typing">
+            <div className="typing__bubble">
+                <span className="typing__dot" />
+                <span className="typing__dot" />
+                <span className="typing__dot" />
+            </div>
+        </div>
+    )
+}
+
+export default TypingIndicator
