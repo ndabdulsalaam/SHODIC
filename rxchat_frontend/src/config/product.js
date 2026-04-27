1bfc137a7fef79df3365f2c2e27cc6ffcb686201
Nurudeen Abdulsalaam <ndabdulsalaam@gmail.com>
2026-04-27 16:35:04 +0100

    Update/Add rxchat_frontend/src/config/product.js

---
+++ rxchat_frontend/src/config/product.js
@@ -0,0 +1,11 @@
+export const PRODUCT = Object.freeze({
+    slug: 'rxchat',
+    name: 'RxChat',
+    apiNamespace: 'rxchat',
+    frontendDomain: 'rxchat.fildah.com',
+})
+
+export function productApiPath(path = '') {
+    const normalizedPath = path.startsWith('/') ? path : `/${path}`
+    return `/${PRODUCT.apiNamespace}${normalizedPath}`
+}
