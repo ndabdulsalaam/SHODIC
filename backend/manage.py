bf084c2bc7f8b9483222452e4efafe11f81ddbc1
Nurudeen Abdulsalaam <ndabdulsalaam@gmail.com>
2026-05-03 10:05:39 +0100

    chore: update manage.py for revised settings module path

--- backend/manage.py
+++ backend/manage.py
@@ -6,7 +6,10 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.dev')
+    from config.env import configure_environment
+
+    configure_environment()
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.dev")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
